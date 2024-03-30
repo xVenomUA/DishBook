fb7449a59ac186b93b7b721498b2e29cd61d8009
Pohrebniak Andrian <pogra29@gmail.com>
2024-03-30 18:59:15 +0200

    Done

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+    return (
+        <main>
+        <h1 className="text-4xl text-slate font-bold text-center mt-10">Something went wrong</h1>
+        </main>
+    );
+}
+    
